9004e803e6574d041caf870d20951a3f7966abc6
jonathandowdell <jdowde2@wgu.edu>
2021-04-19 11:18:46 -0500

    Renaming

--- Base ScannerTests/Base_ScannerTests.swift
+++ Base ScannerTests/Base_ScannerTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Binary_Scanner
 
-class Binary_ScannerTests: XCTestCase {
+class Base_ScannerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- Base ScannerUITests/Base_ScannerUITests.swift
+++ Base ScannerUITests/Base_ScannerUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class Binary_ScannerUITests: XCTestCase {
+class Base_ScannerUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
